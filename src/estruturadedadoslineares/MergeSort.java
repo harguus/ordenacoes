66da0ae276b6626c137fb429aa1c2b4d9f2be307
Thiago Cristiano Tavares <thiagocristiano.tavares@gmail.com>
2018-11-05 22:01:02 -0300

    repositório criado, merge e quick implementados

---
+++ src/estruturadedadoslineares/MergeSort.java
@@ -0,0 +1,80 @@
+package estruturadedadoslineares;
+
+public class MergeSort {
+
+    void merge(int arr[], int esquerdo, int meio, int direito){
+        int subEsquerdo = meio - esquerdo + 1;
+        int subDireito = direito - meio;
+
+        // arrays demporários
+        int auxEsquerdo[] = new int [subEsquerdo];
+        int auxDireito[] = new int [subDireito];
+
+        //copia para os arrays temporários
+        for (int i=0; i<subEsquerdo; ++i)
+            auxEsquerdo[i] = arr[esquerdo + i];
+        for (int j=0; j<subDireito; ++j)
+            auxDireito[j] = arr[meio + 1+ j];
+
+
+        // junta os arrays
+        int i = 0, j = 0;
+        int k = esquerdo;
+
+        while (i < subEsquerdo && j < subDireito){
+            if (auxEsquerdo[i] <= auxDireito[j]){
+                arr[k] = auxEsquerdo[i];
+                i++;
+            }
+            else{
+                arr[k] = auxDireito[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < subEsquerdo){
+            arr[k] = auxEsquerdo[i];
+            i++;
+            k++;
+        }
+
+        while (j < subDireito){
+            arr[k] = auxDireito[j];
+            j++;
+            k++;
+        }
+    }
+
+    void sort(int arr[], int esquerda, int direita){
+        if (esquerda < direita){
+
+            int meio = (esquerda + direita) / 2;
+
+            sort(arr, esquerda, meio);
+            sort(arr , meio + 1, direita);
+
+            merge(arr, esquerda, meio, direita);
+        }
+    }
+
+    static void printArray(int arr[]){
+        int n = arr.length;
+        for (int i=0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        int arr[] = {12, 11, 13, 5, 6, 7};
+
+        System.out.println("\nArray para ser ordenado");
+        printArray(arr);
+
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(arr, 0, arr.length-1);
+
+        System.out.println("\nArray ordenado");
+        printArray(arr);
+    }
+}
---
+++ src/estruturadedadoslineares/QuickSort.java
@@ -0,0 +1,58 @@
+package estruturadedadoslineares;
+
+public class QuickSort {
+
+    //https://www.geeksforgeeks.org/quick-sort/
+
+    int particao(int arr[], int menor, int maior){
+        int pivor = arr[maior];
+        int iMenor = ( menor - 1 );
+        for (int j = menor; j < maior; j++){
+            // Se o elemento atual for menor que ou igual a pivor
+            if ( arr[j] <= pivor ){
+                iMenor++; // incremento o índice do elemento menor
+
+                // troca o menor pelo elemento arr[j]
+                int aux = arr[iMenor];
+                arr[iMenor] = arr[j];
+                arr[j] = aux;
+            }
+        }
+
+        //troca o menor + 1
+        int aux = arr[iMenor + 1];
+        arr[iMenor + 1] = arr[maior];
+        arr[maior] = aux;
+        return iMenor + 1;
+    }
+
+    void sort(int arr[], int menor, int maior){
+        if (menor < maior){
+            int pivor = particao(arr, menor, maior);
+            sort(arr, menor, pivor-1);
+            sort(arr, pivor+1, maior);
+        }
+    }
+
+    static void printArray(int arr[]){
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+
+
+    public static void main(String args[]){
+        int arr[] = {2, 17, 8, 5, 1, 5};
+        int n = arr.length;
+
+        System.out.println("\nArray para ser ordenado");
+        printArray(arr);
+
+        QuickSort quickSort = new QuickSort();
+        quickSort.sort(arr, 0, n - 1);
+
+        System.out.println("\nArray ordenado");
+        printArray(arr);
+    }
+}
