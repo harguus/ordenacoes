df805796891078030e37d3561399e96f11c25667
Thiago Cristiano Tavares <thiago.tavares@academico.ifrn.edu.br>
2018-11-07 16:10:26 -0300

    tempo de execução

--- src/estruturadedadoslineares/QuickSort.java
+++ src/estruturadedadoslineares/QuickSort.java
@@ -1,5 +1,12 @@
 package estruturadedadoslineares;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class QuickSort {
 
     int particao(int arr[], int menor, int maior){
@@ -40,17 +47,53 @@ public class QuickSort {
     }
 
 
-    public static void main(String args[]){
-        int arr[] = {2, 17, 8, 5, 1, 5};
-        int n = arr.length;
+    public static void main(String args[]) throws FileNotFoundException {
 
-        System.out.println("\nArray para ser ordenado");
-        printArray(arr);
+        String[] files = new String[9];
+        files[0] = "teste_1000_1.dat";
+        files[1] = "teste_1000_2.dat";
+        files[2] = "teste_1000_3.dat";
+        files[3] = "teste_10000_1.dat";
+        files[4] = "teste_10000_2.dat";
+        files[5] = "teste_10000_3.dat";
+        files[6] = "teste_100000_1.dat";
+        files[7] = "teste_100000_2.dat";
+        files[8] = "teste_100000_3.dat";
 
-        QuickSort quickSort = new QuickSort();
-        quickSort.sort(arr, 0, n - 1);
+        int[] tamanho = new int[9];
+        tamanho[0] = 1001;
+        tamanho[1] = 1001;
+        tamanho[2] = 1001;
+        tamanho[3] = 10001;
+        tamanho[4] = 10001;
+        tamanho[5] = 10001;
+        tamanho[6] = 100001;
+        tamanho[7] = 100001;
+        tamanho[8] = 100001;
 
-        System.out.println("\nArray ordenado");
-        printArray(arr);
+        //laço para gerar os vetores
+        for (int i=0;i<9;i++){
+
+            int arr[] = new int[tamanho[i]];
+            int count = 0;
+            Scanner scanner = new Scanner(new FileReader("/home/hargus/ifrn/lineares/ordenacoes/inputs/" + files[i]));
+
+            while (scanner.hasNext()) {
+
+                int valor = scanner.nextInt();
+                arr[count] = valor;
+                count++;
+            }
+
+
+            QuickSort quickSort = new QuickSort();
+            long start = System.currentTimeMillis();
+            quickSort.sort(arr, 0, arr.length - 1);
+            long tempo = System.currentTimeMillis() - start;
+
+            System.out.println(files[i]+ " : " + tempo);
+
+        }
+
     }
 }
